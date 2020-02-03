7120b48d91bccb94fc9a8a4d4a8476a5d871c5da
ygy <zero0_1one@163.com>
2020-02-03 22:35:05 +0800

    排版

--- test/test.js
+++ test/test.js
@@ -58,130 +58,130 @@ describe('config', function() {
     }
   })
 
-    its(N, '<< and <<=', function() {
-      for (const func of randomFunctions) {
-        let n = randomInt(0, 53)
-        let num = func()
-        let binary = num.toString(2)
-        let lnum = new LargeNumber(num)
+  its(N, '<< and <<=', function() {
+    for (const func of randomFunctions) {
+      let n = randomInt(0, 53)
+      let num = func()
+      let binary = num.toString(2)
+      let lnum = new LargeNumber(num)
 
-        assert.equal(slice(binary, 0, binary.length, n), lnum['<<'](n).binary(), `${binary}(${binary.length}) << ${n}`)
-        lnum['<<='](n)
-        assert.equal(slice(binary, 0, binary.length, n), lnum.binary(), `${binary}(${binary.length}) <<= ${n}`)
-      }
-    })
+      assert.equal(slice(binary, 0, binary.length, n), lnum['<<'](n).binary(), `${binary}(${binary.length}) << ${n}`)
+      lnum['<<='](n)
+      assert.equal(slice(binary, 0, binary.length, n), lnum.binary(), `${binary}(${binary.length}) <<= ${n}`)
+    }
+  })
 
-    its(N, '>>> and >>>=', function() {
-      for (const func of randomFunctions) {
-        let n = randomInt(0, 53)
-        let num = func()
-        let binary = num.toString(2)
-        let lnum = new LargeNumber(num)
+  its(N, '>>> and >>>=', function() {
+    for (const func of randomFunctions) {
+      let n = randomInt(0, 53)
+      let num = func()
+      let binary = num.toString(2)
+      let lnum = new LargeNumber(num)
 
-        assert.equal(slice(binary, 0, binary.length - n), lnum['>>>'](n).binary(), `${binary}(${binary.length}) >>> ${n}`)
-        lnum['>>>='](n)
-        assert.equal(slice(binary, 0, binary.length - n), lnum.binary(), `${binary}(${binary.length}) >>>= ${n}`)
-      }
-    })
+      assert.equal(slice(binary, 0, binary.length - n), lnum['>>>'](n).binary(), `${binary}(${binary.length}) >>> ${n}`)
+      lnum['>>>='](n)
+      assert.equal(slice(binary, 0, binary.length - n), lnum.binary(), `${binary}(${binary.length}) >>>= ${n}`)
+    }
+  })
 
-    its(N / 10, '| and |=', function() {
-      for (const func1 of randomFunctions) {
-        for (const func2 of randomFunctions) {
-          let num1 = func1()
-          let num2 = func2()
-
-          let binary1 = num1.toString(2).padStart(53, '0')
-          let binary2 = num2.toString(2).padStart(53, '0')
-
-          let lnum1 = new LargeNumber(num1)
-          let lnum2 = new LargeNumber(num2)
-          let binary = ''
-          for (let i = 0; i < 53; i++) {
-            binary += binary1[i] == '1' || binary2[i] == '1' ? '1' : '0'
-          }
-
-          assert.equal(binary, lnum1['|'](num2).binary(), `${binary1}  | ${binary2}`)
-          lnum1['|='](num2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  |= ${binary2}`)
-
-          lnum1 = new LargeNumber(num1)
-          assert.equal(binary, lnum1['|'](lnum2).binary(), `${binary1}  | ${binary2}`)
-          lnum1['|='](lnum2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  |= ${binary2}`)
-        }
-      }
-    })
-
-    its(N / 10, '& and &=', function() {
-      for (const func1 of randomFunctions) {
-        for (const func2 of randomFunctions) {
-          let num1 = func1()
-          let num2 = func2()
-
-          let binary1 = num1.toString(2).padStart(53, '0')
-          let binary2 = num2.toString(2).padStart(53, '0')
-
-          let lnum1 = new LargeNumber(num1)
-          let lnum2 = new LargeNumber(num2)
-          let binary = ''
-          for (let i = 0; i < 53; i++) {
-            binary += binary1[i] == '1' && binary2[i] == '1' ? '1' : '0'
-          }
-
-          assert.equal(binary, lnum1['&'](num2).binary(), `${binary1}  & ${binary2}`)
-          lnum1['&='](num2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  &= ${binary2}`)
-
-          lnum1 = new LargeNumber(num1)
-          assert.equal(binary, lnum1['&'](lnum2).binary(), `${binary1}  & ${binary2}`)
-          lnum1['&='](lnum2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  &= ${binary2}`)
-        }
-      }
-    })
-
-    its(N / 10, '^ and ^=', function() {
-      for (const func1 of randomFunctions) {
-        for (const func2 of randomFunctions) {
-          let num1 = func1()
-          let num2 = func2()
-
-          let binary1 = num1.toString(2).padStart(53, '0')
-          let binary2 = num2.toString(2).padStart(53, '0')
-
-          let lnum1 = new LargeNumber(num1)
-          let lnum2 = new LargeNumber(num2)
-          let binary = ''
-          for (let i = 0; i < 53; i++) {
-            binary += binary1[i] != binary2[i] ? '1' : '0'
-          }
-          assert.equal(binary, lnum1['^'](num2).binary(), `${binary1}  ^ ${binary2}`)
-          lnum1['^='](num2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  ^= ${binary2}`)
-
-          lnum1 = new LargeNumber(num1)
-          assert.equal(binary, lnum1['^'](lnum2).binary(), `${binary1}  ^ ${binary2}`)
-          lnum1['^='](lnum2)
-          assert.equal(binary, lnum1.binary(), `${binary1}  ^= ${binary2}`)
-        }
-      }
-    })
-
-    its(N, 'not and notSelf', function() {
-      for (const func1 of randomFunctions) {
+  its(N / 10, '| and |=', function() {
+    for (const func1 of randomFunctions) {
+      for (const func2 of randomFunctions) {
         let num1 = func1()
+        let num2 = func2()
 
         let binary1 = num1.toString(2).padStart(53, '0')
+        let binary2 = num2.toString(2).padStart(53, '0')
 
         let lnum1 = new LargeNumber(num1)
+        let lnum2 = new LargeNumber(num2)
         let binary = ''
         for (let i = 0; i < 53; i++) {
-          binary += binary1[i] == '0' ? '1' : '0'
+          binary += binary1[i] == '1' || binary2[i] == '1' ? '1' : '0'
         }
 
-        assert.equal(binary, lnum1.not().binary(), `~= ${binary1}`)
-        lnum1.notSelf()
-        assert.equal(binary, lnum1.binary(), `~${binary1}`)
+        assert.equal(binary, lnum1['|'](num2).binary(), `${binary1}  | ${binary2}`)
+        lnum1['|='](num2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  |= ${binary2}`)
+
+        lnum1 = new LargeNumber(num1)
+        assert.equal(binary, lnum1['|'](lnum2).binary(), `${binary1}  | ${binary2}`)
+        lnum1['|='](lnum2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  |= ${binary2}`)
       }
-    })
+    }
+  })
+
+  its(N / 10, '& and &=', function() {
+    for (const func1 of randomFunctions) {
+      for (const func2 of randomFunctions) {
+        let num1 = func1()
+        let num2 = func2()
+
+        let binary1 = num1.toString(2).padStart(53, '0')
+        let binary2 = num2.toString(2).padStart(53, '0')
+
+        let lnum1 = new LargeNumber(num1)
+        let lnum2 = new LargeNumber(num2)
+        let binary = ''
+        for (let i = 0; i < 53; i++) {
+          binary += binary1[i] == '1' && binary2[i] == '1' ? '1' : '0'
+        }
+
+        assert.equal(binary, lnum1['&'](num2).binary(), `${binary1}  & ${binary2}`)
+        lnum1['&='](num2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  &= ${binary2}`)
+
+        lnum1 = new LargeNumber(num1)
+        assert.equal(binary, lnum1['&'](lnum2).binary(), `${binary1}  & ${binary2}`)
+        lnum1['&='](lnum2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  &= ${binary2}`)
+      }
+    }
+  })
+
+  its(N / 10, '^ and ^=', function() {
+    for (const func1 of randomFunctions) {
+      for (const func2 of randomFunctions) {
+        let num1 = func1()
+        let num2 = func2()
+
+        let binary1 = num1.toString(2).padStart(53, '0')
+        let binary2 = num2.toString(2).padStart(53, '0')
+
+        let lnum1 = new LargeNumber(num1)
+        let lnum2 = new LargeNumber(num2)
+        let binary = ''
+        for (let i = 0; i < 53; i++) {
+          binary += binary1[i] != binary2[i] ? '1' : '0'
+        }
+        assert.equal(binary, lnum1['^'](num2).binary(), `${binary1}  ^ ${binary2}`)
+        lnum1['^='](num2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  ^= ${binary2}`)
+
+        lnum1 = new LargeNumber(num1)
+        assert.equal(binary, lnum1['^'](lnum2).binary(), `${binary1}  ^ ${binary2}`)
+        lnum1['^='](lnum2)
+        assert.equal(binary, lnum1.binary(), `${binary1}  ^= ${binary2}`)
+      }
+    }
+  })
+
+  its(N, 'not and notSelf', function() {
+    for (const func1 of randomFunctions) {
+      let num1 = func1()
+
+      let binary1 = num1.toString(2).padStart(53, '0')
+
+      let lnum1 = new LargeNumber(num1)
+      let binary = ''
+      for (let i = 0; i < 53; i++) {
+        binary += binary1[i] == '0' ? '1' : '0'
+      }
+
+      assert.equal(binary, lnum1.not().binary(), `~= ${binary1}`)
+      lnum1.notSelf()
+      assert.equal(binary, lnum1.binary(), `~${binary1}`)
+    }
+  })
 })
